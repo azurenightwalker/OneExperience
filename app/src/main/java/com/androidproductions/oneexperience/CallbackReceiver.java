d81aacc1f0df28cd91836266ee021ac4e96ce139
Scott Jones <scott.jones@x-labsystems.co.uk>
2014-10-22 17:33:06 +0100

    Split out callback receiver

---
+++ app/src/main/java/com/androidproductions/oneexperience/CallbackReceiver.java
@@ -0,0 +1,19 @@
+package com.androidproductions.oneexperience;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+import com.lge.qpair.api.r1.QPairConstants;
+
+public class CallbackReceiver extends BroadcastReceiver {
+    public CallbackReceiver() {
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        String errorMessage = intent.getStringExtra(QPairConstants.EXTRA_CAUSE);
+        Log.e(OEConstants.TAG, errorMessage);
+    }
+}
